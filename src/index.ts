555356d574a1bec74358b1874ddb9a0291b62566
Thiago Garcia <thiagogarciafraga99@gmail.com>
2022-06-20 13:01:49 +0000

    Testes em index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-const disciplina: string = 'Programação Orientada A Objetos III';
-console.log(disciplina);
+type user =  {
+    name: string;
+    password: string;
+    id: number;
+}
+
+
+const usuario: user = {
+    name: 'Thiago',
+    password: '123456',
+    id: 1
+}
+
+function somaNumeros(num1: number, num2: number): void{
+
+    console.log(`Soma de ${num1} + ${num2} = ${num1+num2}`);
+
+}
+
+
+somaNumeros(1,2);
