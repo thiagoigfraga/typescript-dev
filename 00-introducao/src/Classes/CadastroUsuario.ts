b25c7f633c8f09c2c112cf527c4735f229cb88cf
Thiago Fraga <thiagogarciafraga99@gmail.com>
2022-06-25 19:25:17 -0300

    Atualizações de classes
    na classe CadastroUsuario.ts e no index.ts

--- 00-introducao/src/Classes/CadastroUsuario.ts
+++ 00-introducao/src/Classes/CadastroUsuario.ts
@@ -1,48 +1,52 @@
-import {Endereco} from "./Endereco";
-import {Sexo} from "./Sexo";
+import { Endereco } from './Endereco';
+import { Sexo } from './Sexo';
 
-
-export class CadastroUsuario{
-
+export class CadastroUsuario {
     private _nome: string;
     private _cpf: number;
     private _rg: number;
     private _sexo: Sexo;
     private _endereco: Endereco;
 
-
-    constructor(nome: string, cpf: number,rg: number,sexo: Sexo, endereco: Endereco) {
-     this._nome = nome;
-     this._cpf =  cpf;
-     this._rg =  rg;
-     this._sexo = sexo;
-     this._endereco = endereco;
+    constructor(
+        nome: string,
+        cpf: number,
+        rg: number,
+        sexo: Sexo,
+        endereco: Endereco
+    ) {
+        this._nome = nome;
+        this._cpf = cpf;
+        this._rg = rg;
+        this._sexo = sexo;
+        this._endereco = endereco;
     }
 
-    get nome(): string{
+    get nome(): string {
         return this._nome;
     }
 
-    set nome(nome:string){
+    set nome(nome: string) {
         this._nome = nome;
     }
 
-    get cpf(): number{
+    get cpf(): number {
         return this._cpf;
     }
 
-    set cpf(cpf:number){
+    set cpf(cpf: number) {
         this._cpf = cpf;
     }
 
-    get rg(): number{
+    get rg(): number {
         return this._rg;
     }
 
-    set rg(rg:number){
+    set rg(rg: number) {
         this._rg = rg;
     }
 
-
-
+    get endereco(): Endereco {
+        return this._endereco;
+    }
 }
--- 00-introducao/src/index.ts
+++ 00-introducao/src/index.ts
@@ -1,25 +1,26 @@
-import {CadastroUsuario} from "./Classes/CadastroUsuario";
-import {Sexo} from "./Classes/Sexo";
-import {Endereco} from "./Classes/Endereco";
+import { CadastroUsuario } from './Classes/CadastroUsuario';
+import { Sexo } from './Classes/Sexo';
+import { Endereco } from './Classes/Endereco';
 
-const usuario1 = new CadastroUsuario('Thiago', 11234567891, 123456789, Sexo.Masculino,
-    new Endereco('Das laranjeiras',
-        12,
-        1234567,
-        'Das americas',
-        'Guarapuava')
+const usuario1 = new CadastroUsuario(
+    'Thiago',
+    11234567891,
+    123456789,
+    Sexo.Masculino,
+    new Endereco('Das laranjeiras', 12, 1234567, 'Das americas', 'Guarapuava')
 );
 
-const usuario2 = new CadastroUsuario('Lucas', 2318762377, 9876543, Sexo.Masculino,
-    new Endereco('Das Rosas',
-        64,
-        87654322,
-        'Centro',
-        'Pato Branco')
+const usuario2 = new CadastroUsuario(
+    'Lucas',
+    2318762377,
+    9876543,
+    Sexo.Masculino,
+    new Endereco('Das Rosas', 64, 87654322, 'Centro', 'Pato Branco')
 );
 
+console.log(usuario1.nome);
+console.log(usuario1.cpf);
+console.log(usuario1.rg);
+console.log(usuario1.endereco.bairro);
 
-console.log(usuario1.nome)
-console.log(usuario2.nome)
-
-
+console.log(usuario2.nome);
