fc479d7a9058277bc66f350fa5351b2467916968
thiagoigfraga <thiagogarciafraga99@gmail.com>
2022-07-08 09:25:51 -0300

    Update index.ts

--- 02-abstractFactory-ex/index.ts
+++ 02-abstractFactory-ex/index.ts
@@ -1,5 +1,6 @@
 
 import { Client } from "./classes/Client";
+import { IAiqfomeDelivery } from "./classes/factories/IAiqFomeDelivery";
 import { IFoodDelivery } from "./classes/factories/IFoodDelivery";
 import { IDeliveryFactory } from "./interfaces/IDeliveryFactory";
 
@@ -8,5 +9,6 @@ let factory: IDeliveryFactory;
 factory = new IFoodDelivery();
 
 const client = new Client(new IFoodDelivery());
+//const client = new Client(new IAiqfomeDelivery());
 
 client.startDelivery();
